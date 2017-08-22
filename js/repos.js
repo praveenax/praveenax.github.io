c0c3ef09197d8566e3d51a8b8228ea0909736e5a
Praveen <xav.axp@gmail.com>
2017-08-22 10:33:03 +0530

    Added language in GitHub api

--- js/repos.js
+++ js/repos.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
         $.each(data, function (key, val) {
 
 //            if (val.owner.login == "praveenax") {
-                items.push("<li class='repo_item' id='" + key + "'><a target='_blank' href='" + val.html_url + "'>" + val.name + "</a></li>");
+                items.push("<div class='repo_item' id='" + key + "'><a target='_blank' href='" + val.html_url + "'>" + val.name.charAt(0).toUpperCase() + val.name.substr(1).toLowerCase() + "</a><br><br> Language:"+val.language+"</div>");
 //            }
 
         });
 
-        $("<ul/>", {
+        $("<div/>", {
             "class": "repo-list",
             html: items.join("")
         }).appendTo("#repos");
